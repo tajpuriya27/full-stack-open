cb234cb10ec3dd538e252e22f78788d06d9c4980
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-24 23:21:52 +0545

    feat: 2.4 course information, step8 completed
    I want to render component with map function and want to use empty tag, how can I render them with keys. Don't want extra div in my application

--- part2/Readme.md
+++ part2/Readme.md
@@ -14,3 +14,7 @@ This part is again sub-divided into 5 sub-categories:
 - We can make our own code snippet in VS code. [For more](https://code.visualstudio.com/docs/editor/userdefinedsnippets#_creating-your-own-snippets)
 - Recomended [YouTube playlist](https://www.youtube.com/playlist?list=PL0zVEGEvSaeEd9hlmCXrk5yUyqUag-n84) to strengthen the concept of functional programming in JS.  
   **Warning**: _Before continuing further, must watch atleast first three videos from the playlist_
+
+## Arosed question:
+
+- `EXE2.4` **Doubt::** Line 29, Course component; how can I keep the key attribute to empty tag. **NB** I don't want extra `div` in my application.
--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,9 +1,11 @@
 const Header = (props) => {
+  // console.log("header component", props);
   const { name } = props.course;
-  return <h1>{name}</h1>;
+  return <h2>{name}</h2>;
 };
 
 const Content = (props) => {
+  // console.log('Content component', props);
   const { parts } = props.course;
   // console.log(parts);
   return parts.map((element) => <Part key={element.id} part={element} />);
@@ -20,14 +22,17 @@ const Total = (props) => {
 };
 
 const Course = (props) => {
-  const { course } = props;
-  return (
-    <>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </>
-  );
+  // console.log("course component", props);
+  const { courses } = props;
+  return courses.map((element) => {
+    return (
+      <div key={element.id}>
+        <Header course={element} />
+        <Content course={element} />
+        <Total course={element} />
+      </div>
+    );
+  });
 };
 
 const Part = ({ part }) => (
@@ -37,31 +42,55 @@ const Part = ({ part }) => (
 );
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      <Course courses={courses} />
     </>
   );
 };
