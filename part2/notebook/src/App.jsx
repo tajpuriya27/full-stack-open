3c795f2148e91fee24dca56416d2d82ede0260a2
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-25 16:38:11 +0545

    feat: 2.7 notebook, step2 completed

--- part2/notebook/src/App.jsx
+++ part2/notebook/src/App.jsx
@@ -8,7 +8,10 @@ const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPersons(persons.concat({ name: newName }));
+    const isDublicate = persons.filter((person) => person.name === newName);
+    isDublicate.length
+      ? alert(`${newName} is already added to phonebook`)
+      : setPersons(persons.concat({ name: newName }));
     setNewName("");
   };
 
