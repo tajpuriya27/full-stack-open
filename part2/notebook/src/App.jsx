440cfc92172909cc8e03531481496f6f4ea8d617
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-07-25 16:51:44 +0545

    feat: 2.8 notebook, step3 completed
    Used different states for name and phone number - need to refactor if possible

--- part2/notebook/src/App.jsx
+++ part2/notebook/src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "98768484" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
-  const handleChange = (e) => setNewName(e.target.value);
+  const handleNameChange = (e) => setNewName(e.target.value);
+  const handlePhoneChange = (e) => setNewPhone(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const isDublicate = persons.filter((person) => person.name === newName);
     isDublicate.length
       ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat({ name: newName }));
+      : setPersons(persons.concat({ name: newName, phone: newPhone }));
     setNewName("");
+    setNewPhone("");
   };
 
   return (
@@ -20,7 +25,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          phone: <input value={newPhone} onChange={handlePhoneChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -28,7 +36,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((element) => (
-        <div key={element.name}>{element.name}</div>
+        <div key={element.name}>
+          {element.name} {element.phone}
+        </div>
       ))}
     </div>
   );
