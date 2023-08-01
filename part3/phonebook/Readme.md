3cd31741daef4bd29c15e2990a965400fafb756e
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-01 15:34:48 +0545

    feat: 3.1 phonebook backend step1 completed
    See readme to understand what I did

--- part3/phonebook/Readme.md
+++ part3/phonebook/Readme.md
@@ -1,1 +1,7 @@
-# 
+# Fullstack Open - Part3
+
+### Exe 3.1 Completed.
+
+- Installed express & nodemon.
+- Changed script in `package.json`.
+- Send hardcoded person as api.
---
+++ part3/phonebook/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Exe 3.1 completed.</h1> <a href='http://localhost:3001/api/persons'>Click here for API</a>"
+  );
+});
+
+app.get("/api/persons", (req, res) => {
+  res.send(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => console.log(`App is running on ${PORT}`));
