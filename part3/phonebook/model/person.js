d1dbb36e9e67a8655b075162a137facad0d66ca2
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-17 11:59:44 +0545

    feat:2.22 lint configuration completed

--- part3/phonebook/model/person.js
+++ part3/phonebook/model/person.js
@@ -38,24 +38,3 @@ personSchema.set("toJSON", {
 });
 
 module.exports = mongoose.model("Person", personSchema);
-
-// if (process.argv.length === 3) {
-//   Person.find({}).then((result) => {
-//     result.forEach((person) => {
-//       console.log(person);
-//     });
-//     mongoose.connection.close();
-//   });
-// }
-
-// if (process.argv.length === 5) {
-//   const person = new Person({
-//     name: process.argv[3],
-//     number: process.argv[4],
-//   });
-
-//   person.save().then((result) => {
-//     console.log("person saved!");
-//     mongoose.connection.close();
-//   });
-// }
