e7088424dce67c24913cc067411db7eb7789a1c3
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-21 15:02:13 +0545

    feat: 4.4 helper function test written and passed

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return Array.isArray(blogs) ? 1 : 0;
 };
 
+const totalLikes = (blogs) => {
+  let likeCount = 0;
+  if (blogs.length > 1) {
+    blogs.forEach((blog) => {
+      likeCount += blog.likes;
+    });
+  } else if (blogs.length === 1) {
+    likeCount = blogs[0].likes;
+  }
+
+  return likeCount;
+};
 module.exports = {
   dummy,
+  totalLikes,
 };
