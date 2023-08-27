c0d79d618a5944bebee17633a0ef6b0c787bbc4b
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-08-27 23:13:25 +0545

    refactor: change then-catch to async-await

--- part5/blog-frontend/src/services/blogs.js
+++ part5/blog-frontend/src/services/blogs.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3003/api/blogs";
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
 };
 
 export default { getAll };
