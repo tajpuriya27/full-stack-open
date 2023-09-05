e55fc49b81c6db16025d265607f35225699d6d52
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-05 21:16:29 +0545

    feat: 5.10 sorting functionality added

--- part5/blog-frontend/src/App.jsx
+++ part5/blog-frontend/src/App.jsx
@@ -183,14 +183,16 @@ const App = () => {
           </p>
 
           {blogForm()}
-          {blogs.map((blog) => (
-            <Blog
-              key={blog.id}
-              blog={blog}
-              updateLikes={() => updateLikes(blog)}
-              blogOwner={user.name}
-            />
-          ))}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog
+                key={blog.id}
+                blog={blog}
+                updateLikes={() => updateLikes(blog)}
+                blogOwner={user.name}
+              />
+            ))}
         </div>
       )}
     </div>
