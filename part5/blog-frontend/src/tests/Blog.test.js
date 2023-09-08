028094db1b918a09f938a31b45bc46d65907e6a5
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-08 13:32:36 +0545

    5.14 Blog-list test #2 added

--- part5/blog-frontend/src/tests/Blog.test.js
+++ part5/blog-frontend/src/tests/Blog.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import Blog from "../components/Blog";
 
 test("renders blog title and blog author but not likes and url", () => {
@@ -20,4 +20,25 @@ test("renders blog title and blog author but not likes and url", () => {
   expect(div).toHaveTextContent("Jest Library");
   expect(element).toBeNull();
   expect(screen.queryByText("url")).toBeNull();
+});
+
+test.only("Likes and Url will be shown when toggled", async () => {
+  const blog = {
+    title: "A test-case",
+    author: "Jest Library",
+    url: "https://localhost:3000/",
+    likes: 99,
+  };
+  const container = render(<Blog blog={blog} />).container;
+
+  const user = userEvent.setup();
+  const button = screen.getByText("show");
+  await user.click(button);
+  screen.debug();
+
+  const div = container.querySelector(".blog-div");
+  expect(div).toHaveTextContent("https://localhost:3000/");
+  expect(div).toHaveTextContent("99");
+  expect(screen.queryByText("Likes")).toBeDefined();
+  expect(screen.queryByText("url")).toBeDefined();
 });
