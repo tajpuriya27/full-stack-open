e0a3c42fc38bb5b1bc7a9fa1dec00403759bbf79
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-16 10:25:45 +0545

    fix: updated the server data and it's services to it's required standard

--- part6/anecdotes-redux/src/services/anecdotes.js
+++ part6/anecdotes-redux/src/services/anecdotes.js
@@ -6,3 +6,9 @@ export const getAnecdotes = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
+
+export const createAnecdote = async (anecdoteContent) => {
+  const response = await axios.post(baseUrl, anecdoteContent);
+  console.log("clg", response);
+  return response.data;
+};
