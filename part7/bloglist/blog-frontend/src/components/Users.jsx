1ba6cda07631557f4acba1c6c23b7077ff0f244c
tajpuriya27 <tajpuriyasunil27@gmail.com>
2023-09-20 07:50:22 +0545

    feat: 7.14 user view added to app

--- part7/bloglist/blog-frontend/src/components/Users.jsx
+++ part7/bloglist/blog-frontend/src/components/Users.jsx
@@ -1,9 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { initializeUsers } from "../reducers/userReducer";
 
 const Users = () => {
-  const blogs = useSelector((state) => state.blogs);
-  console.log(blogs);
-  return <h1>User component</h1>;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(initializeUsers());
+  }, []);
+
+  const users = useSelector((state) => state.users);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>User</th>
+          <th>blogs created</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((n) => (
+          <tr key={n.id}>
+            <td>{n.name}</td>
+            <td>{n.blogs.length}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Users;
